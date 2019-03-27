b8f98c9c80bf0537b135bc787c2917eb4590885b
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-26 22:32:47 -0600

    Single server only with Node.js

---
+++ node-server.js
@@ -0,0 +1,15 @@
+
+// web server only with node
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        //...
+    }
+
+    if (req.url === '/api/courses') {
+        //...
+    }
+});
+
+server.listen(3000);
