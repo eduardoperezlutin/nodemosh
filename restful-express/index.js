2f8b7fbb43a361023074ac3db5fbe6bbf8758760
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-27 23:17:01 -0600

    Optimize way of return the error messages

--- restful-express/index.js
+++ restful-express/index.js
@@ -22,11 +22,7 @@ app.get('/api/courses', (req, res) => {
 app.post('/api/courses', (req, res) => {
     // apply validations
     const { error } = validateCourse(req.body);
-    if (error) {
-        // 400 Bad Request
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
     const course = {
         id: courses.length + 1,
@@ -37,22 +33,13 @@ app.post('/api/courses', (req, res) => {
 });
 
 app.put('/api/courses/:id', (req, res) => {
-    // Look up the course
-    // If not exist, return 404
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found');
+    if (!course) return res.status(404).send('The course with the given ID was not found');
 
-    // Validate
-    // If invalid, return 400 - Bad Request
     const { error } = validateCourse(req.body);
 
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(error.details[0].message);
 
-    // Update Course
-    // Return the updated course
     course.name = req.body.name;
     res.send(course);
 });
@@ -68,7 +55,7 @@ function validateCourse(course) {
 
 app.delete('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found');
+    if (!course) return res.status(404).send('The course with the given ID was not found');
     
     const index = courses.indexOf(course);
     courses.splice(index, 1);
@@ -78,7 +65,7 @@ app.delete('/api/courses/:id', (req, res) => {
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found');
+    if (!course) return res.status(404).send('The course with the given ID was not found');
     res.send(course);
 });
 
