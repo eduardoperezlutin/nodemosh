4c0e291240829ff634995145bc9a254e415436bd
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-03-27 21:51:47 -0600

    Return json object

--- restful-express/index.js
+++ restful-express/index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+app.get('/api/courses', (req, res) => {
+    res.send([1, 2, 3]);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
